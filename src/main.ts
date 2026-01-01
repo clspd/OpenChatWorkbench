cf9568cda8778bd8a1f91f000dc8e6c5ce3fc060
shc0743 <shc0743@outlook.com>
2026-01-01 11:53:05 +0800

    Revert "Initial AI chat application implementation"
    This reverts commit abc2afd0661e401523c6f2e5581d9700ad1dda26.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import './assets/main.css'
-import 'ant-design-vue/dist/reset.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import Antd from 'ant-design-vue'
 
 import App from './App.vue'
 import router from './router'
@@ -12,6 +10,5 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(Antd)
 
 app.mount('#app')
