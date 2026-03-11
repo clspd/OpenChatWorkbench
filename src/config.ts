d68670fcc3738162bbc7cf534b254f680962dc07
shc0743 <shc0743@outlook.com>
2026-03-12 05:46:49 +0800

    perf(statistics): replace custom worker with free service (#8)

--- src/config.ts
+++ src/config.ts
@@ -5,8 +5,10 @@ export const cookie_consent_updated_at = 1771172142;
 export const domain_name_main_root = "openchatworkbench.com";
 export const domain_name_stable = "chat.openchatworkbench.com";
 export const domain_name_canary = "canary.openchatworkbench.com";
+export const domain_name_backup = [];
 export const privacy_policy_href = "/resource/privacy.html";
 export const analytics_base_url = "https://analytics.openchatworkbench.com/api/v1/";
+export const trace_info_url = "https://analytics.openchatworkbench.com/cdn-cgi/trace";
 export const domain_name_root = (window.location.hostname.endsWith(domain_name_main_root) ? domain_name_main_root : window.location.hostname); // for cookies-based data; note that must be correctly set to work in development environment
 
 
