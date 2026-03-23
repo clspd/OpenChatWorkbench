c9a09f2ab75880abbee20b59565153d94cc772a9
shc0743 <shc0743@outlook.com>
2026-03-24 02:48:37 +0800

    fix(cacheUtil): fix a JavaScript error

--- src/utils/cacheUtil.ts
+++ src/utils/cacheUtil.ts
@@ -6,15 +6,16 @@ export async function ForceDiscardCache() {
     if (await isServiceWorkerActive()) {
         const c = (window as any).appInitConfig;
         const cache = await caches.open(c.CACHE_PREFIX + c.CACHE_VERSION);
+	const u = new URL(window.location.href);
         await cache.delete(new URL("/", window.location.href), { ignoreSearch: true });
-        await cache.delete(new URL(window.location.href), { ignoreSearch: true });
-        const newResp = await fetch(window.location.href, { cache: 'no-store' });
+        await cache.delete(u, { ignoreSearch: true });
+        const newResp = await fetch(u, { cache: 'no-store' });
         await cache.put(u, newResp);
     }
     else {
         for (let i = 0; i < 2; i++) {
             document.cookie = `sys.operation.clearCache=yes; path=/; max-age=3600; secure`;
-            await (await fetch(location.href, { cache: 'no-store' })).arrayBuffer();
+            await (await fetch(window.location.href, { cache: 'no-store' })).arrayBuffer();
         }
     }
 }
